55922cf7f8f84848b47f05500a013b652fbce29e
nominalrune <nominalrune.yokoyama@gmail.com>
2023-03-19 16:58:36 +0900

    CalendarEntry定義フライング

---
+++ app/Models/CalendarEntry.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Models;
+
+class CalendarEntry
+{
+    public $title;
+    public $entry_type;
+    public $entry_id;
+    public $user_id;
+    public $description;
+    public $start_at;
+    public $start;
+    public $end_at;
+    public $end;
+    public $url;
+    public $status;
+    public $created_at;
+    public $updated_at;
+
+}
